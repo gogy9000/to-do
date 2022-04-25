53673fbee84a36a0c48a3a379d917ef3de6301a6
gogy9000 <goropashnyj9000@gmail.com>
2022-04-25 15:24:58 +0500

    dg

---
+++ build/precache-manifest.37be479d0c1a347d216e8b06ed385cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f272e9ff205710c504a0243c00bb6d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "806fb0bfdd9719695b61",
+    "url": "/static/css/main.dfca60c8.chunk.css"
+  },
+  {
+    "revision": "f48ef34b7cfae2f7938e",
+    "url": "/static/js/2.28e9b7fe.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.28e9b7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806fb0bfdd9719695b61",
+    "url": "/static/js/main.9f9cff69.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
